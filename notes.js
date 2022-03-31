ab542251e957e436fba78df0c48dd3eb5f6d1fc6
Lena Namiki <lena.namiki@gmail.com>
2022-03-31 13:06:01 +0900

    added notes

--- notes.js
+++ notes.js
@@ -1,3 +1,10 @@
 // Creating a React App
 // $ npx create-react-app file-name --use-npm (<use npm instead of yarn to install everything)
 // $ npm start (this will run app in localhost:3000)
+
+// ReactDOM.render() takes in 2 arguments: element to create, where to render
+// React.createElement() takes in 3 arguments: tagName, properties, children
+ReactDOM.render(
+    React.createElement("h1", null, "Hello!"),
+    document.getElementById("root")
+);
